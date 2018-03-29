c5a7c58e36da930130df6bd033cd4319a0125bfa
Dawn <lu923418334@gmail.com>
2018-03-29 16:00:02 +0800

    修改API调用参数

--- app/src/main/java/deu/hlju/dawn/studentattendance/network/Api.java
+++ app/src/main/java/deu/hlju/dawn/studentattendance/network/Api.java
@@ -10,20 +10,28 @@ import deu.hlju.dawn.studentattendance.bean.FaceSetCreateResult;
 import deu.hlju.dawn.studentattendance.bean.FaceSetDetailResult;
 import deu.hlju.dawn.studentattendance.bean.FaceSetRemoveResult;
 import io.reactivex.Observable;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 import retrofit2.http.QueryMap;
 
 public interface Api {
+    @FormUrlEncoded
     @POST("detect")
-    Observable<FaceDetectResult> getFaceDetect(@QueryMap Map<String, String> fields);
+    Observable<FaceDetectResult> getFaceDetect(@FieldMap Map<String, String> fields);
+    @FormUrlEncoded
     @POST("faceset/create")
-    Observable<FaceSetCreateResult> createFaceset(@QueryMap Map<String, String> fields);
+    Observable<FaceSetCreateResult> createFaceset(@FieldMap Map<String, String> fields);
+    @FormUrlEncoded
     @POST("faceset/removeface")
-    Observable<FaceSetRemoveResult> removeFaceset(@QueryMap Map<String, String> fields);
+    Observable<FaceSetRemoveResult> removeFaceset(@FieldMap Map<String, String> fields);
+    @FormUrlEncoded
     @POST("faceset/addface")
-    Observable<FaceSetAddResult> addFaceset(@QueryMap Map<String, String> fields);
+    Observable<FaceSetAddResult> addFaceset(@FieldMap Map<String, String> fields);
+    @FormUrlEncoded
     @POST("faceset/getdetail")
-    Observable<FaceSetDetailResult> detailFaceset(@QueryMap Map<String, String> fields);
+    Observable<FaceSetDetailResult> detailFaceset(@FieldMap Map<String, String> fields);
+    @FormUrlEncoded
     @POST("search")
-    Observable<FaceSearchResult> searchFace(@QueryMap Map<String, String> fields);
+    Observable<FaceSearchResult> searchFace(@FieldMap Map<String, String> fields);
 }
