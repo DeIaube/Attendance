5196ce6f3df962877b5370d7aa1ded8520d94458
Dawn <lu923418335@gmail.com>
2018-04-01 22:32:51 +0800

    删除无用注释

--- app/src/main/java/deu/hlju/dawn/studentattendance/ui/schedule/ScheduleActivity.java
+++ app/src/main/java/deu/hlju/dawn/studentattendance/ui/schedule/ScheduleActivity.java
@@ -40,14 +40,6 @@ public class ScheduleActivity extends BaseActivity implements ScheduleContract.V
         mPresenter = new SchedulePresenter(this, this);
     }
 
-//    private void addList() {
-//        mList.add(new TimeTableModel(1, 2,1,"财务报表分析", "3-105"));
-//        mList.add(new TimeTableModel(3, 4,1,"审计实务", "3-444"));
-//        mList.add(new TimeTableModel(5, 6,1,"财务管理实务", "3-231"));
-//        mList.add(new TimeTableModel(2, 3,2,"财务管理实务", "3-543"));
-//        mList.add(new TimeTableModel(5, 8,2,"审计实务", "3-333"));
-//    }
-
     public static void start(Context context) {
         context.startActivity(new Intent(context, ScheduleActivity.class));
     }
