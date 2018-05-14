eb419a4576347fa5cfabf827f46f65d62f4a4040
Dawn <lu923418335@gmail.com>
2018-05-14 15:36:14 +0800

    用户增加查看课程功能

--- app/src/main/java/deu/hlju/dawn/studentattendance/ui/MainActivity.java
+++ app/src/main/java/deu/hlju/dawn/studentattendance/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.LinearLayout;
 
 import deu.hlju.dawn.studentattendance.ui.console.ConsoleActivity;
 import deu.hlju.dawn.studentattendance.ui.camera.CameraActivity;
+import deu.hlju.dawn.studentattendance.ui.schedule.ScheduleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,22 +19,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(ll);
         ll.setOrientation(LinearLayout.VERTICAL);
 
-        Button btn8 = new Button(this);
-        btn8.setText("摄像头预览");
-        ll.addView(btn8);
+        Button btn1 = new Button(this);
+        btn1.setText("考勤");
+        ll.addView(btn1);
 
-        Button btn10 = new Button(this);
-        btn10.setText("跳转控制台");
-        ll.addView(btn10);
+        Button btn2 = new Button(this);
+        btn2.setText("查看课程表");
+        ll.addView(btn2);
 
-        btn8.setOnClickListener(new View.OnClickListener() {
+
+        Button btn3 = new Button(this);
+        btn3.setText("跳转控制台");
+        ll.addView(btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CameraActivity.start(MainActivity.this);
             }
         });
 
-        btn10.setOnClickListener(new View.OnClickListener() {
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ScheduleActivity.start(MainActivity.this);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ConsoleActivity.start(MainActivity.this);
